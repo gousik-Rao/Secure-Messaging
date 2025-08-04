77b7d95976839e80684340b189e1bea1a4f4f14c
Gousik <gousikthason@gmail.com>
2025-08-04 18:58:25 +0530

    Minimal changes

--- src/main/java/com/military/controller/UserLoginController.java
+++ src/main/java/com/military/controller/UserLoginController.java
@@ -3,8 +3,8 @@ package com.military.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.military.dto.AuthResponseDTO;
 import com.military.dto.LoginReqDTO;
 import com.military.exceptions.InvalidCredentialsException;
-import com.military.security.JWTUtil;
+import com.military.security.JwtUtils;
 import com.military.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
@@ -22,23 +22,26 @@ import jakarta.validation.Valid;
 @RestController @RequestMapping("/api/auth")
 public class UserLoginController {
 
-	@Autowired
+	@Autowired @Lazy
 	private UserService userService;
 	
+	@Autowired @Lazy
+	private JwtUtils jwtUtil;
+	
 	@PostMapping("/login")
-    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginReqDTO loginRequest, BindingResult result) {                  
-
-        if (result.hasErrors())
-        	throw new IllegalArgumentException(result.getFieldError().getDefaultMessage());
+    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginReqDTO loginRequest) {                  
 
         AuthResponseDTO userDTO = userService.validatingUser(loginRequest.email(), loginRequest.password());
-        
+       	System.out.println("Resolved userDTO: " + userDTO);
+
         if (userDTO == null || userDTO.username() == null)
                 throw new InvalidCredentialsException("Invalid username or password");
 
-        	String token = JWTUtil.generateToken(userDTO.username());
-        	return ResponseEntity.ok(Map.of("token", token, "user", userDTO.username()));
-    }
+       	String token = jwtUtil.generateToken(userDTO.username());
+       	
+        return ResponseEntity.ok(Map.of("token", token, "user", userDTO.username()));
+        	
+	}
 	
 	@PostMapping("/logout")
 	public ResponseEntity<?> logout(HttpSession session) {
@@ -46,13 +49,5 @@ public class UserLoginController {
 		return ResponseEntity.ok(Map.of("message", "Logged out successfully"));
 	}
 }
-
-
-
-
-
-
-
-
 
 
